5aa78c965ee11fb90ce4e81043c0cdb7686e243d
Yuvraj Sachdeva <yuvrajsach@gmail.com>
2025-01-24 18:14:25 +0530

    Hashing

---
+++ Hashing/Maps/ValidAnagram.java
@@ -0,0 +1,11 @@
+// Given two strings s and t, return true if t is an anagram of s, and
+// false otherwise. An anagram is a word or phrase formed by rearranging
+// the letters of a different word or phrase, typically using all the 
+// original letters exactly once.
+// Input - s = "heart", t = "earth" -> Output - true.
+// Input - s = "listen", t = "silent" -> Output - true.
+// Input - s = "hello", t = "world" -> Output - false.
+
+public class ValidAnagram {
+    
+}
