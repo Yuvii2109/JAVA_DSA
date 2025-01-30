1db5ee899ece59846911082b52ab48d24e522cf4
Yuvraj Sachdeva <yuvrajsach@gmail.com>
2025-01-30 20:30:00 +0530

    Tries

---
+++ Tries/LongestWordInDictionary.java
@@ -0,0 +1,13 @@
+// Given an array of strings words representing an English Dictionary, return the longest word in words that can be built one character at a time by other words in words. If there is more than one possible answer, return the longest word with the smallest lexicographical order. If there is no answer, return the empty string. Note that the word should be built from left to right with each additional character being added to the end of a previous word.
+
+// Sample Input 1 - words = ["w","wo","wor","worl","world"]
+// Sample Output 1 - "world"
+// The word "world" can be built one character at a time by "w", "wo", "wor", and "worl".
+
+// Sample Input 2 - words = ["a","banana","app","appl","ap","apply","apple"]
+// Sample Output 2 - "apple"
+// Both "apply" and "apple" can be built from other words in the dictionary. However, "apple" is lexicographically smaller than "apply". 
+
+public class LongestWordInDictionary {
+    // Same question as that of longest word with all prefixes...
+}
