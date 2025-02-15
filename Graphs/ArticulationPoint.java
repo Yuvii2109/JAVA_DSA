e92a2f69b8d3a78f35e049a1048ae591068e83b6
Yuvraj Sachdeva <yuvrajsach@gmail.com>
2025-02-15 23:10:07 +0530

    LeetCode

---
+++ Graphs/ArticulationPoint.java
@@ -0,0 +1,6 @@
+// A vertex in an undirected connected graph is an articulation point(or cut vertex) if removing it(and edges through it) increases the connected component of the graph.
+// Ancestor - A node that was discovered before current node in DFS, is the ancestor of current.
+
+public class ArticulationPoint {
+    
+}
