b62b0203b4d967a8e9eb1cb8e6ae508b17d25f8d
Yuvraj Sachdeva <yuvrajsach@gmail.com>
2025-03-21 23:21:12 +0530

    DP

--- Dynamic Programming/EditDistance.java
+++ Dynamic Programming/EditDistance.java
@@ -25,5 +25,49 @@
 */
 
 public class EditDistance {
-    
+    public static void printDP(int[][] dp){
+        for(int i = 0; i < dp.length; i++){
+            for(int j = 0; j < dp[0].length; j++){
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static int minSteps(String s1, String s2){
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=m; j++){
+                if(i == 0){
+                    dp[i][j] = j;
+                }else if(j == 0){
+                    dp[i][j] = i;
+                }
+            }
+        }
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    // Last Character same - Jitna choti wali problem ko solve hone main steps lagenge utne hi steps badi wali problem ko solve hone main lagenge
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    // Last Character different - 3 options - Insert, Delete, Replace
+                    int insert = dp[i][j-1] + 1; // Last letter same banane ke liye same letter doosre string main add kardia
+                    int delete = dp[i-1][j] + 1; // Last letter same banane ke liye ek string se aakhri letter delete krdia 
+                    int replace = dp[i-1][j-1] + 1; // Last letter same banane ke liye ek string ka letter replace krdia 
+                    dp[i][j] = Math.min(Math.min(insert, delete), replace);
+                }
+            }
+        }
+        System.out.println("DP Table - ");
+        printDP(dp);
+        return dp[n][m];
+        // Time Complexity - O(n*m)
+    }
+    public static void main(String[] args) {
+        String word1 = "intention";
+        String word2 = "execution";
+        System.out.println("Minimum number of operations required to convert str1 to str2 - " + minSteps(word1, word2));
+    }
 }
