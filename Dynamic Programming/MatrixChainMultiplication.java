3c8cb73b7e77956467c1fc29f0cf0fabb617071d
Yuvraj Sachdeva <yuvrajsach@gmail.com>
2025-04-14 00:10:21 +0530

    DP

--- Dynamic Programming/MatrixChainMultiplication.java
+++ Dynamic Programming/MatrixChainMultiplication.java
@@ -36,6 +36,38 @@ public class MatrixChainMultiplication {
         dp[i][j] = minCost;
         return minCost;
     }
+    // Ise toh ignore hi karde bhai...
+    public static int MCMTabulation(int arr[]){
+        int n = arr.length;
+        int[][] dp = new int[n][n];
+        for(int i = 0; i < n; i++){
+            dp[i][i] = 0;
+        }
+        for(int length = 2; length < n; length++){
+            for(int i = 1; i < n - length + 1; i++){
+                int j = i + length - 1;
+                dp[i][j] = Integer.MAX_VALUE;
+                for(int k = i; k < j; k++){
+                    int cost1 = dp[i][k];
+                    int cost2 = dp[k+1][j];
+                    int cost3 = arr[i-1]*arr[k]*arr[j];
+                    int finalCost = cost1 + cost2 + cost3;
+                    dp[i][j] = Math.min(dp[i][j], finalCost);
+                }
+            }
+        }
+        System.out.println("DP Table so formed - ");
+        printDP(dp);
+        return dp[1][n-1];
+    }
+    public static void printDP(int[][] dp){
+        for(int i = 0; i < dp.length; i++){
+            for(int j = 0; j < dp[0].length; j++){
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4, 3};
         int n = arr.length;
@@ -45,6 +77,7 @@ public class MatrixChainMultiplication {
             Arrays.fill(dp[i], -1);
         }
         System.out.println("Minimum cost of scalar multiplications is (Memoization)- " + MCMMemoization(arr, 1, n-1, dp));
+        System.out.println("Minimum cost of scalar multiplications is (Tabulation)- " + MCMTabulation(arr));
     }
 }
 
